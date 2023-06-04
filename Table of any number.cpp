16f49bca5c326e73641cf0bc0335df1243ef9ea5
Ashish756357 <126260804+Ashish756357@users.noreply.github.com>
2023-06-04 08:11:19 +0530

    Create Table of any number.cpp

---
+++ Table of any number.cpp
@@ -0,0 +1,15 @@
+// table of n number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int n;
+    cout<<"Enter the number of which yo[u want table: ";
+    cin>>n;
+    for(i=1;i<=10;i++){
+        cout<<i*n<<endl;
+    }
+    return 0;
+}
