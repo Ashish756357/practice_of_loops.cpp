b0fc929badf424548dff98b47a2746db712f0f71
Ashish756357 <126260804+Ashish756357@users.noreply.github.com>
2023-06-04 09:46:45 +0530

    Create power on a number.cpp

---
+++ power on a number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;\
+    long long int power;
+    cout<<"Enter the value :";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        power=i*i;
+    }
+    cout<<power;
+    return 0;
+}
